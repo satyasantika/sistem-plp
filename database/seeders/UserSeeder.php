da194ce2a0d85506d1e1afd1a9f6aed96a1a1ae3
satyasantika <satyasantika@unsil.ac.id>
2022-08-17 00:27:03 +0700

    add lectures, student seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -22,13 +22,32 @@ class UserSeeder extends Seeder
         while (($data = fgetcsv($csvData, 555, ',')) !== false) {
             if (!$transRow) {
                 User::create([
-                    'username' => $data[0],
-                    'name'     => $data[1],
-                    'phone' => $data[2],
-                    'email'    => $data[3],
-                    'password' => Hash::make('dosen'.$data[0]),
+                    'username'  => $data[0],
+                    'name'      => $data[1],
+                    'phone'     => $data[2],
+                    'email'     => $data[3],
+                    'password'  => Hash::make($data[5]),
                     'subject_id' => $data[4],
+                    'address'   => $data[6],
                 ])->assignRole('dosen');
+            }
+            $transRow = false;
+        }
+        fclose($csvData);
+
+        $csvData = fopen(base_path('/database/seeders/csvs/students.csv'), 'r');
+        $transRow = true;
+        while (($data = fgetcsv($csvData, 555, ',')) !== false) {
+            if (!$transRow) {
+                User::create([
+                    'username'  => $data[0],
+                    'name'      => $data[1],
+                    'phone'     => $data[2],
+                    'email'     => $data[3],
+                    'password'  => Hash::make($data[5]),
+                    'subject_id' => $data[4],
+                    'address'   => $data[6],
+                ])->assignRole('mahasiswa');
             }
             $transRow = false;
         }
